e25f0d59cb8b4aa6ccf0c54b064141d235ea3238
user <user@example.com>
2024-11-28 03:03:50 +0300

    add: grpc tools for go

--- bin/gotools.sh
+++ bin/gotools.sh
@@ -1,8 +1,13 @@
-# install go tools
 # debugger
 go install github.com/go-delve/delve/cmd/dlv@latest
+
 # lint tool for helix
 go install github.com/nametake/golangci-lint-langserver@latest
 go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+
 # obfuscation
 go install mvdan.cc/garble@latest
+
+# grpc
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
